af7ea4206a907661b2b735e0af77f7b3ce439590
Justin Kruskie <justin@kruskie.net>
2024-09-10 11:41:14 -0400

    Create ConversionProgram.java

---
+++ ConversionProgram.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class ConversionProgram {
+
+    /*
+    The menu method displays the menu with four options
+    and is used to select which other method should be
+    called next or if the program should close.
+    */
+
+    /*
+    The calcKilometers method returns the kilometers that
+    are equivalent to a specified number of meters entered.
+    */
+
+
+    /*
+    The calcInches method returns the inches that
+    are equivalent to a specified number of meters.
+    */
+
+    /*
+    The calcFeet method returns the feet that
+    are equivalent to a specified number of meters.
+    */
+
+    public static void main(String[] args) {
+        // Variables and scanner
+
+        // Get a distance from the user.
+
+        // Display the menu and process the user's selection until 4 is selected.
+            // Display the menu.
+
+            // Get the user's selection.
+
+            // Validate the user's selection.
+
+                // If the selection is invaldi, ask them again.
+
+            // Process the user's selection. A switch is probably easiest.
+                // What to do if 1 is selected
+                // What to do if 2 is selected
+                // What to do if 3 is selected
+                // What to do if 4 is selected
+            
+    }
+}
